d08eed344a00dc648d87e1ebc706391b5e2990e1
Leomar Marcelo Marschalk <leomarmm1@gmail.com>
2022-06-19 02:09:11 -0300

    Escrita em registrador implementado

--- MCP23S17.c
+++ MCP23S17.c
@@ -1,1 +1,19 @@
 #include "SPI.h"
+#include "digital.h"
+
+void mcp23S17_init()
+{
+	mcp23S17_write_register(0x0A, 0x80); //IOCON.BANK = 1
+}
+uint8_t mcp23S17_read_register(uint8_t addr)
+{
+
+}
+void mcp23S17_write_register(uint8_t addr, uint8_t word)
+{
+	spi_ssel(LOW);		//Ativa dispositivo SPI
+	spi_write(0x40);	//Endereçamento do dispositivo + Bit de escrita
+	spi_write(addr);	//Escreve endereço do registrador
+	spi_write(word);	//Envia byte a ser escrito
+	spi_ssel(HIGH);		//Desativa dispositivo SPI
+}
